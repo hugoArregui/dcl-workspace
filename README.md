87d0d890c34dc7f3b3f9c7b389b30668bb229f40
Hugo Arregui <hugo@decentraland.org>
2022-05-11 13:49:19 -0300

    update build

--- README.md
+++ README.md
@@ -9,16 +9,18 @@
 ./build.sh usage:
 
 --clone: clone projects
+--proto: compile protocol and copy it everywhere
+--link: link libraries to kernel
+--nats: start nats-server
 -b --build: run npm run build on each project
--i --install: run npm ci on each project
--s --start: run npm run start on each project
--p --protocol: compile protocol and copy it everywhere
+-i --install: run npm ci on each project 
+-s --start: run npm run start on each project 
 ```
 
 ## Example
 
-Build only archipelago-service, but start archipelago-service and explorer-bff
+Build only archipelago-service, but start nats, archipelago-service and explorer-bff
 
 ```
-PROJECTS="archipelago-service" ./build.sh --build && PROJECTS="archipelago-service explorer-bff" ./build.sh --start
+./build.sh -b archipelago-service && ./build.sh --nats -s archipelago-service explorer-bff 
 ```
--- build.sh
+++ build.sh
@@ -6,24 +6,30 @@ function usage {
   echo -e "usage: 
 
 --clone: clone projects
+--proto: compile protocol and copy it everywhere
+--link: link libraries to kernel
+--nats: start nats-server
 -b --build: run npm run build on each project
 -i --install: run npm ci on each project 
 -s --start: run npm run start on each project 
--p --protocol: compile protocol and copy it everywhere
 "
 }
 
-
-KERNEL_PATH=${PROJECTS:-"../kernel"}
-PROJECTS=${PROJECTS:-"archipelago-service explorer-bff lighthouse catalys-comms-peer"}
+KERNEL_PATH=${KERNEL_PATH=:-"../kernel"}
+STARTABLE_PROJECTS="archipelago-service explorer-bff lighthouse"
+ALL_PROJECTS="$STARTTABLE_PROJECTS catalyst-comms-peer"
 
 if [ $# -eq 0 ]; then
-    usage 
-    exit 1
+  usage 
+  exit 1
 fi
 
+INSTALL=0
+BUILD=0
+START=0
+
 for arg in "$@"; do
-    case $arg in
+  case $arg in
     --clone )
       git clone git@github.com:decentraland/archipelago-service.git || echo "archipelago-service already cloned"
       git clone git@github.com:decentraland/explorer-bff.git || echo "explorer-bff already cloned"
@@ -31,63 +37,91 @@ for arg in "$@"; do
       git clone git@github.com:decentraland/catalyst-comms-peer.git || echo "catalys-comms-peer already cloned"
       shift
       ;;
+    --proto)
+      pushd proto > /dev/null
+      protoc --plugin=../$KERNEL_PATH/node_modules/ts-protoc-gen/bin/protoc-gen-ts --js_out="import_style=commonjs,binary:." --ts_out="." comms.proto
+      protoc --plugin=../$KERNEL_PATH/node_modules/ts-protoc-gen/bin/protoc-gen-ts --js_out="import_style=commonjs,binary:." --ts_out="." ws.proto
+      protoc --plugin=../$KERNEL_PATH/node_modules/ts-protoc-gen/bin/protoc-gen-ts --js_out="import_style=commonjs,binary:." --ts_out="." bff.proto
+      protoc --plugin=../$KERNEL_PATH/node_modules/ts-protoc-gen/bin/protoc-gen-ts --js_out="import_style=commonjs,binary:." --ts_out="." nats.proto
+      popd > /dev/null
+
+      cp proto/* $KERNEL_PATH/packages/shared/comms/v4/proto
+      cp proto/* explorer-bff/src/controllers/proto/
+      cp proto/* archipelago-service/src/controllers/proto/
+
+      shift 
+      ;;
+    --link)
+      shift 
+      pushd catalyst-comms-peer > /dev/null
+      npm link
+      popd > /dev/null
+
+      pushd $KERNEL_PATH > /dev/null
+      npm link @dcl/catalyst-peer
+      popd > /dev/null
+      ;;
     -i | --install)
-        for project in $PROJECTS; do
-          pushd $project > /dev/null
-          if [ -f "package-lock.json" ]; then
-            npm ci
-          elif  [ -f "yarn.lock" ]; then
-            npx yarn install --frozen-lockfile
-          else
-            npm i
-          fi
-          popd > /dev/null
-        done
-
-        shift
-        ;;
+      INSTALL=1
+      shift
+      ;;
     -b | --build)
-        for project in $PROJECTS; do
-          pushd $project > /dev/null
-          npm run build
-          popd > /dev/null
-        done
-
-        shift 
-        ;;
+      BUILD=1
+      shift 
+      ;;
+    -n | --nats)
+      nats-server &
+      shift 
+      ;;
     -s | --start)
-        nats-server &
-
-        mkdir -p var/log
-
-        for project in $PROJECTS; do
-          pushd $project > /dev/null
-          npm run start > "../var/log/$project.log"  &
-          popd > /dev/null
-        done
-
-        wait
-        shift 
-        ;;
-    -p | --proto)
-        pushd proto > /dev/null
-        protoc --plugin=../$KERNEL_PATH/node_modules/ts-protoc-gen/bin/protoc-gen-ts --js_out="import_style=commonjs,binary:." --ts_out="." comms.proto
-        protoc --plugin=../$KERNEL_PATH/node_modules/ts-protoc-gen/bin/protoc-gen-ts --js_out="import_style=commonjs,binary:." --ts_out="." ws.proto
-        protoc --plugin=../$KERNEL_PATH/node_modules/ts-protoc-gen/bin/protoc-gen-ts --js_out="import_style=commonjs,binary:." --ts_out="." bff.proto
-        protoc --plugin=../$KERNEL_PATH/node_modules/ts-protoc-gen/bin/protoc-gen-ts --js_out="import_style=commonjs,binary:." --ts_out="." nats.proto
-        popd > /dev/null
-
-        cp proto/* $KERNEL_PATH/packages/shared/comms/v4/proto
-        cp proto/* explorer-bff/src/controllers/proto/
-        cp proto/* archipelago-service/src/controllers/proto/
-
-        shift 
-        ;;
+      START=1
+      shift 
+      ;;
+    -*)
+      usage
+      exit 1
+      ;;
     *)
-        usage # run usage function if wrong argument provided
-        exit 1
-        ;;
-    esac
+      break
+      ;;
+  esac
 done
 
 
+PROJECTS=$ALL_PROJECTS
+if [ "$@" ]; then
+  PROJECTS=$@
+fi
+
+for project in $PROJECTS; do
+  pushd $project > /dev/null
+  if [ $INSTALL -eq 1 ]; then
+    if [ -f "package-lock.json" ]; then
+      npm ci
+    elif  [ -f "yarn.lock" ]; then
+      npx yarn install --frozen-lockfile
+    else
+      npm i
+    fi
+  fi
+  if [ $BUILD -eq 1 ]; then
+    npm run build
+  fi
+  popd > /dev/null
+done
+
+if [ $START -eq 1 ]; then
+  mkdir -p var/log
+
+  PROJECTS_TO_START=$STARTABLE_PROJECTS
+  if [ "$@" ]; then
+    PROJECTS_TO_START=$@
+  fi
+
+  for project in $PROJECTS_TO_START; do
+    pushd $project > /dev/null
+    npm run start &> "../var/log/$project.log"  &
+    popd > /dev/null
+  done
+  wait
+fi
